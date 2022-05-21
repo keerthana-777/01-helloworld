63ebd9fe933356ad88905d8a64dcdccaf397742f
Your Name <you@example.com>
2022-05-21 22:37:37 +0530

    committed

---
+++ reactdemos/my-app/src/MyComponent/Car.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Car extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to class component</h2>
+      </div>
+    );
+  }
+}
+
+export default Car;
---
+++ reactdemos/my-app/src/MyComponent/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <h1>Welcome to my Footer Component in Reactjs</h1>
+    </div>
+  );
+}
